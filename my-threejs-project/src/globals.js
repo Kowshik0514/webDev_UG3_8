646ddd736f5c6d2f0cba425b6a8fd5475784786a
Sanjayvarshith <sanjayvarshith21@gmail.com>
2024-10-08 01:34:11 +0530

    .

--- my-threejs-project/src/globals.js
+++ my-threejs-project/src/globals.js
@@ -1,7 +1,5 @@
 // globals.js
 export let chandelier = null;
 export let chandelierBody = null;
-// export let directionalLight = null;
-// export let directionalLight2= null;
 export let stones = []; // Array to store stones and their physics bodies
 
